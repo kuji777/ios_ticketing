76f9dc80dbd2b7adcbd92fdeedc6ed04a8221fa1
Etudiant <etudiant@iMac-9.local>
2019-10-23 14:12:47 +0200

    update

---
+++ TicketManager/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  TicketManager
+//
+//  Created by Etudiant on 23/10/2019.
+//  Copyright © 2019 ESGI. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Task Manager title")
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TicketManager/TaskListView.swift
@@ -0,0 +1,21 @@
+//
+//  TaskListView.swift
+//  TicketManager
+//
+//  Created by Etudiant on 23/10/2019.
+//  Copyright © 2019 ESGI. All rights reserved.
+//
+
+import SwiftUI
+
+struct TaskListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TaskListView_Previews: PreviewProvider {
+    static var previews: some View {
+        TaskListView()
+    }
+}
